d09069f16497401d864e9235ef54a6adf0990654
tutoraddict <rohitdutta907@gmail.com>
2020-03-27 12:01:20 +0530

    basic structure done thanks to Aimtux-Fuzion for source code

---
+++ src/Hacks/ragebot.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+#include "../SDK/IInputSystem.h"
+#include "../SDK/IGameEvent.h"
+
+namespace Ragebot {
+
+    extern std::vector<int64_t> friends;
+    extern int targetAimbot;
+
+    void CreateMove(CUserCmd*);
+    void FireGameEvent(IGameEvent* event);
+	void UpdateValues();
+}
