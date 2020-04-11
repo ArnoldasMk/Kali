1949561ec3c540f65fd83459d5d161e8b5fc6bd0
OtakuLinux <61698074+OtakuLinux@users.noreply.github.com>
2020-04-11 17:08:37 +0300

    add Prisma 2 Case and Shattered Web Case/Knifes

--- src/Utils/util_items.cpp
+++ src/Utils/util_items.cpp
@@ -68,6 +68,10 @@ namespace Util
 			{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFE_WIDOWMAKER).entityName,		i::WEAPON_KNIFE_WIDOWMAKER },
 			{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFE_CSS).entityName,		i::WEAPON_KNIFE_CSS },
 			{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFE_GHOST).entityName,			i::WEAPON_KNIFE_GHOST },
+			{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFE_CORD).entityName,           i::WEAPON_KNIFE_CORD },
+			{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFE_CANIS).entityName,          i::WEAPON_KNIFE_CANIS },
+			{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFE_OUTDOOR).entityName,        i::WEAPON_KNIFE_OUTDOOR },
+			{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFE_SKELETON).entityName,       i::WEAPON_KNIFE_SKELETON },
 			//{ ItemDefinitionIndexMap.at(i::WEAPON_KNIFEGG).entityName,			i::WEAPON_KNIFEGG },
 			{ ItemDefinitionIndexMap.at(i::GLOVE_STUDDED_BLOODHOUND).entityName, 		i::GLOVE_STUDDED_BLOODHOUND},
 			{ ItemDefinitionIndexMap.at(i::GLOVE_CT_SIDE).entityName,			i::GLOVE_CT_SIDE},
@@ -148,6 +152,10 @@ namespace Util
 				case ItemDefinitionIndex::WEAPON_KNIFE_WIDOWMAKER:
 				case ItemDefinitionIndex::WEAPON_KNIFE_CSS:
 				case ItemDefinitionIndex::WEAPON_KNIFE_GHOST:
+				case ItemDefinitionIndex::WEAPON_KNIFE_CORD:
+				case ItemDefinitionIndex::WEAPON_KNIFE_CANIS:
+				case ItemDefinitionIndex::WEAPON_KNIFE_OUTDOOR:
+				case ItemDefinitionIndex::WEAPON_KNIFE_SKELETON:
 				//case ItemDefinitionIndex::WEAPON_KNIFEGG:
 					return true;
 				default:
